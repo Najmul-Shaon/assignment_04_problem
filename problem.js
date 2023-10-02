e72b857dfbffa64974345fd42f7fd32c9fa39951
Najmul shaon <najmul.nh.shaon@gmail.com>
2023-10-02 23:30:54 +0600

    problem 03 solved

--- problem.js
+++ problem.js
@@ -23,3 +23,30 @@ function matchFinder(string1, string2) {
 // console.log(matchFinder("JavaScript", "Code"));
 // console.log(matchFinder("Peter Parker", "Pen"));
 // console.log(matchFinder("Peter Parker", "pet"));
+
+// problem 03
+function sortMaker(arr) {
+  let newArr = [];
+  let arrFirstElement = arr[0];
+  let arrSecondElement = arr[1];
+  if (arrFirstElement < 0 || arrSecondElement < 0) {
+    return "Invalid Input";
+  } else if (arrFirstElement < arrSecondElement) {
+    let temp = arrFirstElement;
+    arrFirstElement = arrSecondElement;
+    arrSecondElement = temp;
+    newArr.push(arrFirstElement);
+    newArr.push(arrSecondElement);
+    return newArr;
+  } else if (arrFirstElement === arrSecondElement) {
+    return "equal";
+  } else {
+    return arr;
+  }
+}
+// console.log("1st", sortMaker([2, 3]));
+// console.log("2nd", sortMaker([4, 2]));
+// console.log("3rd", sortMaker([4, 4]));
+// console.log("4th", sortMaker([1, 2]));
+// console.log("5th", sortMaker([4, -2]));
+
