a8ee059c608a29e603a057b718042a772e2cbc8e
Najmul shaon <najmul.nh.shaon@gmail.com>
2023-10-02 22:21:29 +0600

    problem 02 solved

--- problem.js
+++ problem.js
@@ -12,3 +12,14 @@ function cubeNumber(number) {
 // console.log(cubeNumberResult);
 
 // problem 02
+function matchFinder(string1, string2) {
+  if (typeof string1 !== "string" || typeof string2 !== "string") {
+    return "Please enter valid string.";
+  }
+  const isInclude = string1.includes(string2);
+  return isInclude;
+}
+// console.log(matchFinder("John Doe", "ohn"));
+// console.log(matchFinder("JavaScript", "Code"));
+// console.log(matchFinder("Peter Parker", "Pen"));
+// console.log(matchFinder("Peter Parker", "pet"));
